cbc2fb9b6eb5bd19816db0ca622e292208d4cb1d
Jaqueline Bauleo <jcbauleo@gmail.com>
2021-04-22 11:33:06 -0300

    Separei os commit

--- main.py
+++ main.py
@@ -9,43 +9,52 @@ def conexao():
                                   database="bancodedados")
 
     cursor = connection.cursor()
-    return cursor
+    return cursor, connection
 
 def create_table(variaveis):
 
-    cursor = conexao()
+    #cursor, connection = conexao()
     columns = variaveis.keys()
     values = variaveis.values()
     create_query_columns = []
+    #create_statics = "DROP TABLE IF EXISTS teste; CREATE TABLE teste(id int primary key);"
+    #cursor.execute(create_statics)
+    #print(create_statics)
     for column in columns:
         create_query_columns.append(column + " int")
-    
-    create_query_columns = ", ".join(create_query_columns)
-    
-    create_query = "CREATE TABLE test (id integer primary key, " + create_query_columns + ")"
-    cursor.execute(create_query)
-    cursor.close()
+    create_query_columns = ", " . join(create_query_columns)
+
+    '''for i in columns:
+        create_query = "ALTER TABLE teste ADD COLUMN, "+ list(columns.keys()) + ";"
+        cursor.execute(create_query)
+        cursor.execute("SELECT * FROM teste;")'''
+
+    create_query = "DROP TABLE IF EXISTS teste; CREATE TABLE teste(id int primary key not null, " + create_query_columns + " ); "
+    #cursor.execute(create_query)
+    #connection.commit()
+    #cursor.execute("SELECT * FROM TESTE;")
+    #cursor.close()
     return print("\n", create_query)
 
 
 def inserting_table(variaveis):
-    cursor = conexao()
-    columns = variaveis.keys()
+    #cursor, connection = conexao()
+    columns = variaveis.keys()  
     values = variaveis.values()
     str_values = []
     for value in values:
         str_values.append(str(value))
     # str_values = [str(value) for value in values]
 
-    insert_query = "INSERT INTO test (id, " + ", ".join(columns) + ") "
+    insert_query = "INSERT INTO teste (id, " + ", ".join(columns) + ") "
     insert_query += "VALUES (1, "+ ", ".join(str_values) + ")"
-    cursor.execute(insert_query)
-    cursor.commit()
-    cursor.close()
+    #cursor.execute(insert_query)
+    #connection.commit()
+    #cursor.close()
     return print("\n", insert_query)
 
 def update_table(variaveis):
-    cursor = conexao()
+    #cursor = conexao()
     id = 1
     columns = variaveis.keys()
 
@@ -55,10 +64,10 @@ def update_table(variaveis):
 
     print(new_values)
     new_values = ", ".join(new_values)
-    update_query = "UPDATE test SET " + new_values + " WHERE id = " + str(id)
-    cursor.execute(insert_query)
-    cursor.commit()
-    cursor.close()
+    update_query = "UPDATE teste SET " + new_values + " WHERE id = " + str(id)
+    #cursor.execute(insert_query)
+    #cursor.commit()
+    #cursor.close()
     return print(update_query)
 
 def log():
@@ -72,17 +81,17 @@ def log():
     checkvalue = re.compile(r'T[0-9]*,', re.IGNORECASE)
     #Procura a palavra "commit"
     commit = re.compile(r'commit', re.IGNORECASE)
+    checkpoint = re.compile(r'CKPT', re.IGNORECASE)
+    startCheckpoint = re.compile(r'Start\sCKPT', re.IGNORECASE)
+    endCheckpoint = re.compile(r'End\sCKPT', re.IGNORECASE)
     #Ignora as palavras descritas e coloca as demais em uma lista com .findall
     extracT = re.compile(r'(?!commit\b)(?!CKPT\b)(?!Start\b)\b\w+', re.IGNORECASE)
-    checkpoint = re.compile(r'End', re.IGNORECASE)
     #Utilizado p/ pegar o valor das variaveis
     words = re.compile(r'\w+', re.IGNORECASE)
-    pegaId = re.compile(r',[0-9]*,')
 
 
     #aqui colocar todos os valores iniciais em um vetor um em cada indice['A', '20', 'B', '20'] 
     valores = words.findall(arquivolist[0])
-    #declara um dicionario
     variaveis = {}
 
     
@@ -92,37 +101,66 @@ def log():
         #Valores[i+1] = Valores de cada "Letra"
         variaveis[valores[i]]= valores[i+1]
 
-    del valores
     print("Valores Iniciais\n", variaveis)
     
     create_table(variaveis)
     inserting_table(variaveis)
 
-    end = 0
-    indice = 0
-    for linha in reversed(arquivolist): #Verificar os casos e criar as listas de REDO
-        if commit.search(linha):  #Procura commit
-            #Guarda em REDO todas as transações que realizaram commit
-            REDO.append(extracT.findall(linha)[0])
-        
-         
-            
-    print("\nAplicado REDO:", REDO, "\n")
+    indexEnd = 0
+    indexStart = 0
+    commitAntes = []
+    commitEntre = []
+    commitDepois = []
 
-    getId = []
-    #For que nao pega nem o primeiro, nem o último indice.
-    for j in range(1,len(arquivolist)-1,1):
+    #Procura o Start CKPT e o End CKPT e guarda os índices
+    for i in range(0, len(arquivolist), 1):
+        if startCheckpoint.search(arquivolist[i]):
+            indexStart = i
+        if endCheckpoint.search(arquivolist[i]):
+            indexEnd = i
+    
+    #Procura todos os commits antes do Start CKPT
+    for i in range(0, indexStart):
+        if commit.search(arquivolist[i]):
+            commitAntes.append(extracT.findall(arquivolist[i])[0])
+    
+    #Procura todos os commits entre o Start e o End
+    for i in range(indexStart, indexEnd):
+        if commit.search(arquivolist[i]):
+            commitEntre.append(extracT.findall(arquivolist[i])[0])
+
+    #Procura todos os commits depois do End CKPT
+    for i in range(indexEnd, len(arquivolist)):
+        if commit.search(arquivolist[i]):
+            commitDepois.append(extracT.findall(arquivolist[i])[0])
+    
+    print("Commit Antes: ", commitAntes)
+    print("Commit Entre: ", commitEntre)
+    print("Commit Depois: ", commitDepois)
+
+    '''
+    for linha in reversed(arquivolist): #Verificar os casos e criar as listas de REDO
+            if commit.search(linha):  #Procura commit
+                #Guarda em REDO todas as transações que realizaram commit
+                REDO.append(extracT.findall(linha)[0])
+    print("\nAplicado REDO:", REDO, "\n")'''
+
+    #Aqui está imprimindo certo todas as colunas e os valores
+    print("Var = ", variaveis)
+
+    for j in range(1, len(arquivolist)):
         #Pega a linha nesse formato <T3,1,A,25>
         linha = arquivolist[j]
-        #Pega só as transações de cada linha(se existir)
+        #Pega todas as transações de cada linha
         if (checkvalue.search(linha)):
-            if(extracT.findall(linha)[0] in REDO):
+            #Pega as transações em redo
+            if(extracT.findall(linha)[0] in commitEntre):
+                #variaveis na posição 1, recebe a letra
                 variaveis[words.findall(linha)[1]] = words.findall(linha)[2]
-                
 
     print("Resultado:", variaveis)
     update_table(variaveis)
 
     arquivo.close()
-
+    
 log()
