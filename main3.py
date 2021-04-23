88e14afc58db9c87737b6d584108cef9ce17a208
[Emili] <[emili_willi@hotmail.com]>
2021-04-22 21:01:52 -0300

    final

--- main3.py
+++ main3.py
@@ -17,18 +17,17 @@ def create_table(variaveis):
     columns = variaveis.keys()
     values = variaveis.values()
     create_query_columns = []
-    create_statics = "DROP TABLE IF EXISTS teste; CREATE TABLE teste(id int primary key);"
-    cursor.execute(create_statics)
     for column in columns:
         create_query_columns.append(column + " int")
     create_query_columns = ", " . join(create_query_columns)
 
-    create_query = "DROP TABLE IF EXISTS teste; CREATE TABLE teste(id int primary key not null, " + create_query_columns + " ); "
+    create_query = "DROP TABLE IF EXISTS teste; "
+    cursor.execute(create_query)
+    create_query = "CREATE TABLE teste(id int primary key not null, " + create_query_columns + " ); "
     cursor.execute(create_query)
     connection.commit()
-    cursor.execute("SELECT * FROM TESTE;")
     cursor.close()
-    return print("\n", create_query)
+    return print(create_query)
 
 def inserting_table(variaveis):
     cursor, connection = conexao()
@@ -37,16 +36,15 @@ def inserting_table(variaveis):
     str_values = []
     for value in values:
         str_values.append(str(value))
-
     insert_query = "INSERT INTO teste (id, " + ", ".join(columns) + ") "
-    insert_query += "VALUES (1, "+ ", ".join(str_values) + ")"
+    insert_query += "VALUES (1, "+ ", ".join(str_values) + ");"
     cursor.execute(insert_query)
     connection.commit()
     cursor.close()
-    return print("\n", insert_query)
+    return print(insert_query)
 
 def update_table(variaveis):
-    cursor = conexao()
+    cursor, connection = conexao()
     id = 1
     columns = variaveis.keys()
 
@@ -54,21 +52,18 @@ def update_table(variaveis):
     for column in columns:
         new_values.append(column + "=" + str(variaveis[column]))
 
-    print(new_values)
     new_values = ", ".join(new_values)
     update_query = "UPDATE teste SET " + new_values + " WHERE id = " + str(id)
-    cursor.execute(insert_query)
-    cursor.commit()
+    cursor.execute(update_query)
+    connection.commit()
     cursor.close()
     return print(update_query)
 
 def log():
     
-    arquivo = open('teste03', 'r')
+    arquivo = open('teste_final', 'r')
 
     arquivolist = list(arquivo)     
-    REDO = []                    
-
 
     checkvalue = re.compile(r'T[0-9]*,', re.IGNORECASE)
     commit = re.compile(r'commit', re.IGNORECASE)
@@ -84,7 +79,6 @@ def log():
     for i in range(0,len(valores),2): 
         variaveis[valores[i]]= valores[i+1]
 
-    print("Valores Iniciais\n", variaveis)
     
     create_table(variaveis)
     inserting_table(variaveis)
@@ -117,20 +111,15 @@ def log():
         if commit.search(arquivolist[i]):
             commitDepois.append(extracT.findall(arquivolist[i])[0])
     
-    print("Commit Antes: ", commitAntes)
-    print("Commit Entre: ", commitEntre) #Será o nosso REDO
-    print("Commit Depois: ", commitDepois)
-
-    print(variaveis)
+    #print("Commit Antes: ", commitAntes)
+    print("\nR E D O: ", commitEntre,"\n") #Será o nosso REDO
+    #print("Commit Depois: ", commitDepois)
     for j in range(1, len(arquivolist)):
         linha = arquivolist[j]
         if (checkvalue.search(linha)):
             match = (extracT.findall(linha))
             if(match[0] in commitEntre and variaveis[match[2]] != match[3]):
-                print(match)
                 variaveis[match[2]] = match[3]
-   
-    print("Resultado:", variaveis)
 
     update_table(variaveis)
     arquivo.close()
